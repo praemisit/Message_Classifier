5d32da18c720991095d76a9398fa0d4771f15d04
praemisit <praemisit@mail.de>
2020-01-11 18:18:26 +0100

    Set the correct paths

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///data/DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponseTable', engine)
 
 # load model
-model = joblib.load("models/DisasterResponseModel.pkl")
+model = joblib.load("../models/DisasterResponseModel.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
